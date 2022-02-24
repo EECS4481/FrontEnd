a9ec0d27c3f802e876e82aa3f5d4e1fe77cad252
Tarik <tarikmohd99@gmail.com>
2022-02-24 15:16:14 -0500

    Changed file structure, 1: moved all views to components 2: removed unnecessary boiler plate files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import App from "./Components/App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ReactQueryDevtools } from "react-query/devtools";
@@ -21,7 +20,3 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
